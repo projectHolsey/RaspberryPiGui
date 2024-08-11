38fe78fd144bfb0c52e0ff2fff2a672e6a8f6406
matt <matty-sep@hotmail.com>
2024-08-11 18:29:28 -0400

    Fixed the sunrise  sunset problems

--- index.js
+++ index.js
@@ -7,6 +7,87 @@
  */
 
 
+
+//API ID from Openweathermap.org : Register to get a free API from openweathermap.org
+let oWapi='cc2c427e479c4ce58d22220b7efe0d39'
+//Enter your City as per the format from Openweathermap.org
+let cIty="Boston,US"
+//Choose your units. Change the C to F as required in Line 41
+let uNits="metric"
+// Line 36 concatenates the date form the FULL_URL for openweathermap.org
+
+//For Sunrise and Moonrise get your free Api key from ipgeolocation.io
+let iPgeoAPI="3ff332c420ca4793ac965c97b66625f3"
+//Enter your Location's Latitude
+let iPgeoLAT="42.4154"
+//Enter your locations's Longitude
+let iPgeolong="71.1565"
+
+// Visual crossing API key for forecast - www.visualcrossing.com/
+let ApiKey = 'ME727NEVFDFPU4AE2BNKCZY53'
+
+let tempcond1 = null;
+let handp1 = null;
+let riseData1 = null;
+
+
+
+function getdata () {
+    let full_url = "https://api.openweathermap.org/data/2.5/weather?q="+cIty+"&units="+uNits+"&appid="+oWapi
+    
+    fetch(full_url)
+    .then(response => response.json())
+    .then(data => {
+        let data2 = data['main']
+        let temp1 = data2["temp"]
+        let tempc = String(temp1)+' C' //Change the C to F as required.
+
+        let pressure1 = main['pressure']
+        let pressurep = "P: "+String(pressure1)+' hpa'
+
+        let humid1 = main['humidity']
+        let humidh= "H:"+String(humid1)+' %'
+
+        let wind = "WS: " + String(data['wind']['speed']) +" MPH"
+
+        handp1 = pressurep + "      " + humidh + "      " + wind
+
+        let weather = data['weather']
+        let condition1 = weather [0]['description']
+        let condition2 = String(condition1)
+
+        tempcond1 = tempc + "  " + condition2
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+
+    setInterval(getdata, 600000);
+}
+
+function rise() {
+
+    let full_url = 'https://api.ipgeolocation.io/astronomy?apiKey='+iPgeoAPI+'&lat='+iPgeoLAT+'&long='+iPgeolong
+    
+    fetch(full_url)
+    .then(response => response.json())
+    .then(data => { 
+        console.log(data);
+        let moonrise = data['moonrise']
+        let sunrise = data['sunrise']
+    
+        riseData1= ("Sunrise : " + sunrise + "     Mooonrise : " + moonrise )
+        
+    })
+    .catch((error) => {
+        console.error(error)
+    });
+
+    setInterval(rise, 21600000);
+    // root.after(21600000,rise)
+}
+
+
 // creating a list of all the potential layouts=c
 let layouts = [1,2,3];
 
@@ -43,7 +124,7 @@ function createLayout(number) {
     function createLayout2() {
         let layout2 = document.createElement('div');
         let label2 = document.createElement('label');
-        label2.innerHTML = "Label 2";
+        label2.innerHTML = tempcond1;
         layout2.appendChild(label2);
         return layout2;
     }
@@ -51,7 +132,7 @@ function createLayout(number) {
     function createLayout3() {
         let layout3 = document.createElement('div');
         let label3 = document.createElement('label');
-        label3.innerHTML = "Label 3";
+        label3.innerHTML = riseData1;
         layout3.appendChild(label3);
         return layout3;
     }
@@ -134,9 +215,13 @@ function main() {
 
     // if we want to make this easier, we'll need something that can find the current screen size.
 
+    // start the 2 functions polling for data in background
+    getdata();
+    rise()
+
     const screenHeight = 400;
     const screenWidth = 700;
-    const numberOfSegments = 3;
+    const numberOfSegments = 2;
 
     // If there's only 1 segment, we make it the size of the screen
     if (numberOfSegments == 1){
